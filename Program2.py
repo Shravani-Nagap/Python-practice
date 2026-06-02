6feaf0eddac26203d4995abe7856c5b546a9c745
Shravani-Nagap <shravani.nagap2007@gmail.com>
2026-06-02 12:46:51 +0530

    Program2.py

---
+++ Program2.py
@@ -0,0 +1,12 @@
+#Progarm2:Simple Calculator
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+op = input("Enter operator:")
+if op =='+':
+   print(a+b)
+elif op=='-':
+   print(a-b)
+elif op=='*':
+   print(a*b)
+else op=='/':
+   print(a/b)
